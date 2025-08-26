e6722a13c876540aec72db64337a720cc37872e1
AH <ahmedcs44@gmail.com>
2025-08-26 23:08:06 +0300

    url leadership

--- src/pages/Leadership.tsx
+++ src/pages/Leadership.tsx
@@ -27,7 +27,7 @@ const Leadership: React.FC = () => {
     const fetchLeaders = async () => {
       try {
         setLoading(true);
-        const response = await fetch('/api/team-members');
+        const response = await fetch('https://ebdaadesign.com/api/team-members');
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
